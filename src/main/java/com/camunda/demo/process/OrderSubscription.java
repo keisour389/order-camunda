63cdf3d59f5158ce9d8f3f64e6c2e905b247e512
Tuan Kiet <limit389@gmail.com>
2023-04-21 11:53:36 +0700

    Added mock data and connect to API

--- src/main/java/com/camunda/demo/process/OrderSubscription.java
+++ src/main/java/com/camunda/demo/process/OrderSubscription.java
@@ -1,14 +1,29 @@
 package com.camunda.demo.process;
 
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Component
 public class OrderSubscription {
 
+    private final ZeebeClient zeebeClient;
+
+    private final String URL = "http://localhost:3001/api/v1";
+
     private static Logger logger = LoggerFactory.getLogger(OrderSubscription.class);
+
+    public OrderSubscription(ZeebeClient zeebeClient) {
+        this.zeebeClient = zeebeClient;
+    }
 
     @JobWorker(type="order_subscription")
     public void sendOrderSubscription() {
@@ -25,14 +40,24 @@ public class OrderSubscription {
         logger.info("Processed order successfully");
     }
 
-    @JobWorker(type="request_payment_gateway")
-    public void handlePaymentGatewayRequest() {
+    @JobWorker(type="request_payment_gateway", autoComplete = true)
+    public Map<String, String> handlePaymentGatewayRequest(final ActivatedJob activatedJob) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(URL + "/request-payment-gateway", String.class, String.class);
+        logger.info(String.valueOf(response.getBody()));
+
         logger.info("Handle payment gateway successfully");
+        zeebeClient.newSetVariablesCommand(activatedJob.getProcessInstanceKey())
+                .variables(Map.of("paymentGateway", String.valueOf(response.getBody())))
+                .send().join();
+        return Map.of("paymentType", String.valueOf(response.getBody()));
     }
 
     @JobWorker(type="inform_store")
     public void informStore() {
-        logger.info("inform store successfully");
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(URL + "/inform-store", String.class, String.class);
+        logger.info(String.valueOf(response.getBody()));
     }
 
     @JobWorker(type="request_logistic_vendor")
@@ -40,9 +65,30 @@ public class OrderSubscription {
         logger.info("Requested logistic vendor successfully");
     }
 
-
     @JobWorker(type="order_registration")
     public void sendOrderRegistration() {
         logger.info("Sent order registration successfully");
     }
+
+    @JobWorker(type="order_cancellation")
+    public void sendOrderCancellation() {
+        logger.info("Order has been cancelled successfully");
+    }
+
+    @JobWorker(type="response_from_delivery")
+    public void respondFromDelivery() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(URL + "/response-from-delivery",
+                String.class, String.class);
+        logger.info(String.valueOf(response.getBody()));
+    }
+
+    @JobWorker(type="inform_to_customer")
+    public void informToCustomer() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(URL + "/inform-customer",
+                String.class, String.class);
+        logger.info(String.valueOf(response.getBody()));
+    }
+
 }
